f6bed7cd5ba9ab62f890d9bfaff6600c4213f013
vagrant <vagrant@homestead>
2019-12-05 13:13:09 +0000

    增加 接口请求重试

--- Swoole-Timer.md
+++ Swoole-Timer.md
@@ -110,7 +110,42 @@ $timer = \Swoole\Timer::tick(2000, function () use (&$num) {
 
 **比如跑的一些脚本任务,如果还需要请求第三方接口,接口超时或者请求失败,设置多久重试,并且多少次请求失败停止请求接口** 
 
+```php
+//请求一个外部接口 超时失败,重试机制 简易版
 
+$url = 'http://xx.test';
+$excel = 0;
+$timer = \Swoole\Timer::tick(1000 * 10, function ($timer_id) use ($url, &$excel) {
+    ++$excel;
+    echo "请求接口中" . PHP_EOL;
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_HEADER, 1);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_POST, 1);
+    $post_data = array(
+        "title" => "1290800466",
+        "content" => "3424243243"
+    );
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
+    $data = curl_exec($curl);
+    curl_close($curl);
+    if ($data) {
+        echo '请求成功'.PHP_EOL;
+        \Swoole\Timer::clear($timer_id);
+    } else {
+        if ($excel > 5) {
+            echo '请求失败已达5次,停止请求接口' . PHP_EOL;
+            \Swoole\Timer::clear($timer_id);
+        }else{
+            echo "请求失败,10秒之后重试".PHP_EOL;
+        }
+    }
+
+});
+
+```
 
 ### 联系
 <a href="https://github.com/wuqinqiang/">
--- Timer/swoole-timer.php
+++ Timer/swoole-timer.php
@@ -10,22 +10,58 @@
 //});
 
 
-$num = 10;
-$timer = \Swoole\Timer::tick(2000, function () use (&$num) {
-    echo ++$num . PHP_EOL;
-    \Swoole\Timer::after(5000, function () use (&$num) {
-        $num -= 2;
+//请求一个外部接口 超时失败,重试机制 简易版
 
-          echo date('Y-m-d H:i:s'). ' 时间管理 '.PHP_EOL;
+$url = 'http://xx.test';
+$excel = 0;
+$timer = \Swoole\Timer::tick(1000 * 10, function ($timer_id) use ($url, &$excel) {
+    ++$excel;
+    echo "请求接口中" . PHP_EOL;
 
-        echo $num.PHP_EOL;
-    });
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_HEADER, 1);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_POST, 1);
+    $post_data = array(
+        "title" => "1290800466",
+        "content" => "3424243243"
+    );
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
+    $data = curl_exec($curl);
+    curl_close($curl);
+    if ($data) {
+        echo '请求成功'.PHP_EOL;
+        \Swoole\Timer::clear($timer_id);
+    } else {
+        if ($excel > 5) {
+            echo '请求失败已达5次,停止请求接口' . PHP_EOL;
+            \Swoole\Timer::clear($timer_id);
+        }else{
+            echo "请求失败,10秒之后重试".PHP_EOL;
+        }
+    }
+
 });
 
-\Swoole\Timer::after(8000, function () use ($timer) {
-    \Swoole\Timer::clear($timer);
-});
 
+//$num = 10;
+//$timer = \Swoole\Timer::tick(2000, function () use (&$num) {
+//    echo ++$num . PHP_EOL;
+//    \Swoole\Timer::after(5000, function () use (&$num) {
+//        $num -= 2;
+//
+//        echo date('Y-m-d H:i:s') . ' 时间管理 ' . PHP_EOL;
+//
+//        echo $num . PHP_EOL;
+//    });
+//});
+//
+//
+//\Swoole\Timer::after(8000, function () use ($timer) {
+//    \Swoole\Timer::clear($timer);
+//});
+//
 
 
 
